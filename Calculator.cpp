eb45355b3545c7a5cc4e3748675c39431a2f94dd
Abdul Moiz <rajaabdulmoiz211@gmail.com>
2023-12-05 09:17:46 +0500

    Calculator File Added

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,c,d,e,f;
+	cout<<"Enter First Number : ";
+	cin>>a;
+	cout<<"Enter Second Number : ";
+	cin>>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	cout<<"Sum of Two Number is : "<<c;
+	cout<<endl;
+	cout<<"Subtraction of Two Number is : "<<d;
+	cout<<endl;
+	cout<<"Multiplication of Two Number is : "<<e;
+	cout<<endl;
+	cout<<"Division of Two Number is : "<<f;
+
+}
